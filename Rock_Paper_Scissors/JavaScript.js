6bab10a7cc5cce922be90ed6d815facb1512207e
David Dunkovich <31701641+DavidDunkovich@users.noreply.github.com>
2021-05-05 17:10:19 -0700

    work with dave

--- Rock_Paper_Scissors/JavaScript.js
+++ Rock_Paper_Scissors/JavaScript.js
@@ -1,47 +1,52 @@
-const myArray = ["Rock", "Paper", "Scissors"];
+const gameOptions = ["rock", "paper", "scissors"];
+
+main();
+
+function main() {
+  printValue("Hello welcome to the game!");
+
+  for (let i = 0; i < 3; i++) {
+    const playerChoice = playerPlay();
+    const computerChoice = computerPlay();
+
+    declareWinner(computerChoice, playerChoice);
+    printValue("Finished round: " + (i + 1));
+  }
+
+  printValue("Thanks for playing");
+}
+
+function printValue(value) {
+  console.log(value);
+}
 
 function computerPlay() {
-  return myArray[~~(Math.random() * myArray.length)];
+  const index = Math.floor(Math.random() * gameOptions.length);
+  printValue("Computers choice: " + gameOptions[index]);
+  return gameOptions[index];
 }
 
-console.log(computerPlay().toLowerCase());
+function playerPlay() {
+  const playerChoice = prompt("Rock, Paper, or Scissors");
+  printValue("Players choice: " + playerChoice);
 
-let playerChoice = prompt("Rock, Paper, or Scissors");
-
-function playerPlay() { 
-    return console.log(playerChoice.toLowerCase());
+  return playerChoice.toLowerCase();
 }
 
-playerPlay();
-
-function computerSeleciton() {
-    return computerPlay().toLowerCase();
+function declareWinner(computerChoice, playerChoice) {
+  if (computerChoice == playerChoice) {
+    console.log("Its a tie game");
+  } else if (computerChoice == "scissors" && playerChoice == "paper") {
+    console.log("Computer Wins");
+  } else if (computerChoice == "rock" && playerChoice == "paper") {
+    console.log("Player Wins");
+  } else if (computerChoice == "rock" && playerChoice == "scissors") {
+    console.log("Computer Wins");
+  } else if (computerChoice == "paper" && playerChoice == "scissors") {
+    console.log("Player Wins");
+  } else if (computerChoice == "paper" && playerChoice == "rock") {
+    console.log("Computer Wins");
+  } else if (computerChoice == "scissors" && playerChoice == "rock") {
+    console.log("Player Wins");
+  } else console.log("Not working properly");
 }
-
-function playerSelection() {
-    return playerPlay().toLowerCase();
-}
-
-
-function declareWinner(computerSelection, playerSelection) {
-    computerSelection;
-    playerSelection;
-    if (computerSeleciton == playerSelection) {
-        console.log("Its a tie game");
-    } else if (computerSeleciton == "scissors" && playerSelection == "paper") {
-        console.log("Computer Wins");
-    } else if (computerSeleciton == "rock" && playerSelection == "paper") {
-        console.log("Player Wins");
-    } else if (computerSeleciton == "rock" && playerSelection == "scissors") {
-         console.log("Computer Wins");
-    } else if (computerSeleciton == "paper" && playerSelection == "scissors") {
-         console.log("Player Wins");
-    } else if (computerSeleciton == "paper" && playerSelection == "rock") {
-         console.log("Computer Wins");
-    } else if (computerSeleciton == "scissors" && playerSelection == "rock") {
-        console.log("Player Wins");
-    } else console.log("Not working properly");
-    }
-
-
-declareWinner(computerSeleciton, playerSelection);
