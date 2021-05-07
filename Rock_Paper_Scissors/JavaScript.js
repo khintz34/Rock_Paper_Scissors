b63f229d030fd2977944a223575094793e22c2aa
khintz34 <78268730+khintz34@users.noreply.github.com>
2021-05-07 19:55:01 -0400

    Add files via upload

--- Rock_Paper_Scissors/JavaScript.js
+++ Rock_Paper_Scissors/JavaScript.js
@@ -1,20 +1,63 @@
 const gameOptions = ["rock", "paper", "scissors"];
 
+let computerScore = 0;
+let playerScore = 0;
+let cpuWinRound = "Computer Wins";
+let playerWinRound = "Player Wins";
+let tieRound = "Its a tie game";
+let cpuWinsGame = "Computer WINS THE GAME... Better Luck Next Time";
+let playerWinsGame = "YOU WIN!!! YOU ROCK!!"
+
+
 main();
 
 function main() {
   printValue("Hello welcome to the game!");
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 1000; i++) {
     const playerChoice = playerPlay();
     const computerChoice = computerPlay();
 
-    declareWinner(computerChoice, playerChoice);
+    printValue(declareWinner(computerChoice, playerChoice));
+    gameScore();
     printValue("Finished round: " + (i + 1));
+    printValue("Your Score is: " + playerScore);
+    printValue("Computer Score is: " + computerScore);
+    endCredits();
+
+    function gameScore() {
+      let result = declareWinner(computerChoice, playerChoice);
+    
+      if (result === playerWinRound) {
+        playerScore++;
+      } else if (result === cpuWinRound) {
+        computerScore++;
+      } else { playerScore, computerScore;
+      }
+    }
+
+    function endCredits() {
+      if (playerScore === 5) {
+        console.log(playerWinsGame);
+        return;
+      }
+      if (computerScore === 5) {
+        console.log(cpuWinsGame);
+        return;
+      }
+    }
+  
+
+    if(computerScore === 5 || playerScore ===5) {
+      break;
+    }
   }
 
   printValue("Thanks for playing");
+
 }
+
+
 
 function printValue(value) {
   console.log(value);
@@ -35,18 +78,18 @@ function playerPlay() {
 
 function declareWinner(computerChoice, playerChoice) {
   if (computerChoice == playerChoice) {
-    console.log("Its a tie game");
+    return tieRound;
   } else if (computerChoice == "scissors" && playerChoice == "paper") {
-    console.log("Computer Wins");
+    return cpuWinRound;
   } else if (computerChoice == "rock" && playerChoice == "paper") {
-    console.log("Player Wins");
+    return playerWinRound;
   } else if (computerChoice == "rock" && playerChoice == "scissors") {
-    console.log("Computer Wins");
+    return cpuWinRound;
   } else if (computerChoice == "paper" && playerChoice == "scissors") {
-    console.log("Player Wins");
+    return playerWinRound;
   } else if (computerChoice == "paper" && playerChoice == "rock") {
-    console.log("Computer Wins");
+    return cpuWinRound;
   } else if (computerChoice == "scissors" && playerChoice == "rock") {
-    console.log("Player Wins");
-  } else console.log("Not working properly");
+    return playerWinRound;
+  } else return("Not working properly");
 }
